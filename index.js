ec4caebf39599d0e7404e5cd3b2c7efba705ac37
Thomas Chaplin <tomrchaplin@gmail.com>
2019-11-19 21:04:07 +0000

    Solving matching problem

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 var bodyParser = require('body-parser');
 var passwordHasher = require('password-hash-and-salt');
+var findMatching = require('bipartite-matching');
 
 const app = express();
 const adapter = new FileSync('db.json');
@@ -13,16 +14,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 db.defaults({
 	people: [
-		{id: 0, name: "Thomas", submitted: false, hash: '', possible: [], target: ''},
-		{id: 1, name: "Nicola", submitted: false, hash: '', possible: [], target: ''},
+		{id: 0, name: "Tom", submitted: false, hash: '', possible: [], target: ''},
+		/*{id: 1, name: "Nicola", submitted: false, hash: '', possible: [], target: ''},
 		{id: 2, name: "Inigo", submitted: false, hash: '', possible: [], target: ''},
 		{id: 3, name: "Pratt", submitted: false, hash: '', possible: [], target: ''},
 		{id: 4, name: "Massimo", submitted: false, hash: '', possible: [], target: ''},
 		{id: 5, name: "Ollie C", submitted: false, hash: '', possible: [], target: ''},
 		{id: 6, name: "Ollie M", submitted: false, hash: '', possible: [], target: ''},
-		{id: 7, name: "Anisha", submitted: false, hash: '', possible: [], target: ''},
-		{id: 8, name: "Amrik", submitted: false, hash: '', possible: [], target: ''},
-		{id: 9, name: "Dan", submitted: false, hash: '', possible: [], target: ''}
+		{id: 7, name: "Anisha", submitted: false, hash: '', possible: [], target: ''},*/
+		{id: 1, name: "Amrik", submitted: false, hash: '', possible: [], target: ''},
+		{id: 2, name: "Dan", submitted: false, hash: '', possible: [], target: ''}
 	],
 	all_submitted: false
 }).write();
@@ -38,19 +39,51 @@ function checkIfDone() {
 	db.set('all_submitted', done).write();
 	if(done) {
 		console.log('We are done');
+		computeMatching();
 	} else {
 		console.log('Still not done :(');
 	}
 }
 
+function computeMatching() {
+	var edges = [];
+	var num_people = 0;
+	var db_data = db.get('people').value();
+	db_data.forEach( giver => {
+		num_people = num_people + 1;
+		giver.possible.forEach( recipient => {
+			edges.push([parseInt(giver.id), parseInt(recipient)]);
+		})
+	});
+	var matching = findMatching(num_people,num_people,edges);
+	console.log(matching);
+	matching.forEach( pair => {
+		db.get('people').find({id: pair[0]})
+			.assign({
+				target: pair[1]
+			}).write();
+	});
+}
+
 app.get('/', (req, res) => {
 	console.log(req.path);
-	fs.readFile("views/name-list.tmpl","utf8", (err,tmpl_file) => {
-		var tmpl = combyne(tmpl_file);
-		var db_data = db.get('people').value();
-		var output = tmpl.render(db_data);
-		res.send(output);
-	});
+	done = db.get('all_submitted').value();
+	console.log(done);
+	if(!db.get('all_submitted').value()) {
+		fs.readFile("views/name-list.tmpl","utf8", (err,tmpl_file) => {
+			var tmpl = combyne(tmpl_file);
+			var db_data = db.get('people').value();
+			var output = tmpl.render(db_data);
+			res.send(output);
+		});
+	} else {
+		fs.readFile("views/name-list-2.tmpl","utf8", (err,tmpl_file) => {
+			var tmpl = combyne(tmpl_file);
+			var db_data = db.get('people').value();
+			var output = tmpl.render(db_data);
+			res.send(output);
+		});
+	}
 })
 
 app.get('/form/:id', (req, res) => {
@@ -64,7 +97,7 @@ app.get('/form/:id', (req, res) => {
 				people.push(person);
 			}
 		})
-		var name = db_data[req.params.id].name;
+		var name = db_data[parseInt(req.params.id)].name;
 		var output = tmpl.render({id:req.params.id, name: name, people: people});
 		res.send(output);
 	});
@@ -83,7 +116,6 @@ app.post('/form/:id', (req, res) => {
 		}
 	})
 	passwordHasher(password).hash( (err, hash) => {
-		console.log(hash)
 		db.get('people').find({ id: parseInt(req.params.id)})
 			.assign({
 				submitted: true,
